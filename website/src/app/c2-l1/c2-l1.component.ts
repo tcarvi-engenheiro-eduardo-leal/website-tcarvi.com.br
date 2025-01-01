6d1276558f1cd75034a076f5de5f6526db3bd8bf
tcarvi <tcarvi@me.com>
2025-01-01 10:51:54 -0300

    Update

--- website/src/app/c2-l1/c2-l1.component.ts
+++ website/src/app/c2-l1/c2-l1.component.ts
@@ -19,16 +19,20 @@ export class C2L1Component {
   produtosMidiaticos: Array<Site>;
   constructor() {
     this.projetos = [
-      { title: 'Projeto de Topografia', link: 'https://engenharia-de-fundacao.web.app' },
-      { title: 'Projeto Arquitetônico', link: 'https://engenharia-estrutural.web.app' },
+      { title: 'Projeto de Topografia', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-topografia/tree/main/tutorial/docs' },
+      { title: 'Projeto Arquitetônico', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-arquitetura/tree/main/website/projeto-de-arquitetura/docs' },
       { title: 'Projeto de Acústica', link: 'https://engenharia-acustica.web.app' },
       { title: 'Projeto de Iluminação', link: 'https://projeto-de-iluminacao.web.app' },
-      { title: 'Projeto de Materiais Compósitos', link: 'https://engenharia-estrutural.web.app' },
+      { title: 'Projeto de Materiais Compósitos', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-materiais-compositos/tree/main/tutorial/docs' },
       { title: 'Projeto de Engenharia Estrutural', link: 'https://engenharia-estrutural.web.app' },
       { title: 'Projeto de Engenharia Geotécnica de Fundação', link: 'https://engenharia-de-fundacao.web.app' }
     ];
     this.pesquisas = [
-      { title: 'Análise de Dados e Inteligência Artificial', link: 'https://tcarvi-inteligencia-artificial.web.app' },
+      { title: 'Pesquisa sobre Inteligência Artificial', link: 'https://tcarvi-inteligencia-artificial.web.app' },
+      { title: 'Pesquisa sobre Teoria dos Conjuntos', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-teoria-dos-conjuntos/tree/main/website/docs' },
+      { title: 'Pesquisa sobre Algebra', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-algebra/tree/main/website/docs' },
+      { title: 'Pesquisa sobre Algebra Linear', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-algebra-linear/tree/main/website/docs' },
+
     ];
     this.produtosMidiaticos = [
       { title: 'Artes Visuais', link: 'https://projeto-de-artes-visuais.web.app' },
