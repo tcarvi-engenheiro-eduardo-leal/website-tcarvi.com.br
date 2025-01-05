a23140fbbddf2281317a13e26e23b8ae2331a22d
tcarvi <tcarvi@me.com>
2025-01-05 16:42:21 -0300

    Update

--- website/src/app/c2-l1/c2-l1.component.ts
+++ website/src/app/c2-l1/c2-l1.component.ts
@@ -25,14 +25,15 @@ export class C2L1Component {
       { title: 'Projeto de Iluminação', link: 'https://projeto-de-iluminacao.web.app' },
       { title: 'Projeto de Materiais Compósitos', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/projeto-de-materiais-compositos/tree/main/tutorial/docs' },
       { title: 'Projeto de Engenharia Estrutural', link: 'https://engenharia-estrutural.web.app' },
-      { title: 'Projeto de Engenharia Geotécnica de Fundação', link: 'https://engenharia-de-fundacao.web.app' }
+      { title: 'Projeto de Engenharia Geotécnica de Fundação', link: 'https://engenharia-de-fundacao.web.app' },
     ];
     this.pesquisas = [
-      { title: 'Pesquisa sobre Inteligência Artificial', link: 'https://tcarvi-inteligencia-artificial.web.app' },
-      { title: 'Pesquisa sobre Teoria dos Conjuntos', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-teoria-dos-conjuntos/tree/main/website/docs' },
       { title: 'Pesquisa sobre Algebra', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-algebra/tree/main/website/docs' },
       { title: 'Pesquisa sobre Algebra Linear', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-algebra-linear/tree/main/website/docs' },
-
+      { title: 'Pesquisa sobre Geometria', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-geometria/tree/main/website/docs' },
+      { title: 'Pesquisa sobre Inteligência Artificial', link: 'https://tcarvi-inteligencia-artificial.web.app' },
+      { title: 'Pesquisa sobre Lógica Matemática', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-logica-matematica' },
+      { title: 'Pesquisa sobre Teoria dos Conjuntos', link: 'https://github.com/tcarvi-engenheiro-eduardo-leal/pesquisa-sobre-teoria-dos-conjuntos/tree/main/website/docs' },
     ];
     this.produtosMidiaticos = [
       { title: 'Produto de Artes Visuais', link: 'https://projeto-de-artes-visuais.web.app' },
