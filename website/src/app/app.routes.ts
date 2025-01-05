bee8df69e0cf34b8d155a35f16e520aa336e7d5c
tcarvi <tcarvi@me.com>
2025-01-05 17:09:21 -0300

    Update

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -4,8 +4,7 @@ import { L1Component } from './l-1/l-1.component';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'l1'
+    component: L1Component
   },
   {
     path: 'l1',
